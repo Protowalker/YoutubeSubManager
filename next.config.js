40cf1c7610c2b1d95880ab39028e98e12a25a435
Protowalker <jacksonedwards6@gmail.com>
2021-04-07 22:02:51 -0400

    added npm run export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 module.exports = {
     images: {
         domains: ['yt3.ggpht.com'],
     },
+    assetPrefix: ghPages ? '/YoutubeSubManager/' : '',
 }
