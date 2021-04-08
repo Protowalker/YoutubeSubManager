495557aca71915b9fa6e5d3b7e4de55c7fff2c73
Protowalker <jacksonedwards6@gmail.com>
2021-04-07 22:10:59 -0400

    fix basePath

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
         domains: ['yt3.ggpht.com'],
     },
     assetPrefix: ghPages ? '/YoutubeSubManager/' : '',
-    basePath: ghPages ? '/YoutubeSubManager/' : ''
+    basePath: ghPages ? '/YoutubeSubManager' : ''
 }
