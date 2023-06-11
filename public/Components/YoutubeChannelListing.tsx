4e667f760115e7229b4ccaff82e410e294837046
Jackie Edwards <jackieedwards@milestechnologies.com>
2023-06-11 14:33:51 -0400

    Generate fetch call to unsubscribe from all selected channels

--- public/Components/YoutubeChannelListing.tsx
+++ public/Components/YoutubeChannelListing.tsx
@@ -1,166 +1,156 @@
 import {
-  Avatar,
-  Box,
-  Button,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Image,
-  LinkOverlay,
-  Text,
+    Avatar,
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    Grid,
+    GridItem,
+    Heading,
+    Image,
+    LinkOverlay,
+    Text,
 } from "@chakra-ui/react";
-import { useCallback, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 import useSWR from "swr";
 import { Subscription } from "../Hooks/useSubscriptions";
 import { YoutubeChannel } from "../Interfaces/YoutubeInterfaces";
+import { SelectedSubscriptionContext } from "./YoutubeSubscriptionManager";
 import Video from "./YoutubeVideo";
 
 export default function YoutubeChannelListing(props: {
-  subscription: Subscription;
+    subscription: Subscription;
 }) {
-  const {
-    channel: channelFeed,
-    isLoading,
-    error,
-  } = useChannel(props.subscription.snippet.resourceId.channelId);
+    const {
+        channel: channelFeed,
+        isLoading,
+        error,
+    } = useChannel(props.subscription.snippet.resourceId.channelId);
 
-  const [subscribed, setSubscribed] = useState(true);
-  const [processingOperation, setProcessingOperation] = useState(false);
+    const [subscribed, setSubscribed] = useState(true);
+    const [processingOperation, setProcessingOperation] = useState(false);
 
-  if (isLoading) return <>loading</>;
+    const [selectedSubs, setSubs] = useContext(SelectedSubscriptionContext);
 
-  if (error) return <>{`${error}`}</>;
+    if (isLoading) return <>loading</>;
 
-  return (
-    <Grid
-      templateRows="1"
-      templateColumns="repeat(24, 1fr)"
-      gap="1rem"
-      alignItems="center"
-      justifyItems="center"
-      bgColor="red.700"
-      p="1rem"
-      width="90%"
-    >
-      <GridItem
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-between"
-        colSpan={2}
-        width="100%"
-        as="a"
-        href={channelFeed.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        title={channelFeed.name}
-      >
-        <Avatar src={props.subscription.snippet.thumbnails.default.url} />
-        <Heading size="md" textAlign="center" wordBreak="break-word">
-          {channelFeed.name}
-        </Heading>
-      </GridItem>
-      <GridItem
-        colStart={3}
-        colSpan={12}
-        display="flex"
-        flexDirection="column"
-        gap="1rem"
-        alignItems="start"
-        width="100%"
-      >
-        <Heading size="md">Most Recent Videos</Heading>
-        <Flex gap="1rem" alignItems="center" width="100%">
-          {channelFeed.videos.slice(0, 3).map((video) => (
-            <Video video={video} key={video.url} channelUrl={channelFeed.url} />
-          ))}
-        </Flex>
-      </GridItem>
-      {/* <GridItem
-        colStart={16}
-        colSpan={4}
-        display="flex"
-        width="100%"
-        gap="1rem"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Heading size="md">Last watched by you</Heading>
-        <Video channelUrl={channelFeed.url} video={channelFeed.videos[0]} />
-      </GridItem> */}
-      <GridItem colStart={16}>
-        <Button
-          bgColor={subscribed ? "red.800" : "gray.300"}
-          onClick={(ev) => {
-            if (processingOperation === true) return;
-            setProcessingOperation(true);
-            if (subscribed === true) {
-              unsubscribeFromChannel(props.subscription.id).then(() => {
-                setSubscribed(false);
-                setProcessingOperation(false);
-              });
-            } else {
-              subscribeToChannel(props.subscription).then(() => {
-                setSubscribed(true);
-                setProcessingOperation(false);
-              });
-            }
-          }}
+    if (error) return <>{`${error}`}</>;
+
+    return (
+        <Grid
+            templateRows="1"
+            templateColumns="repeat(24, 1fr)"
+            gap="1rem"
+            alignItems="center"
+            justifyItems="center"
+            bgColor="red.700"
+            p="1rem"
+            width="90%"
         >
-          {processingOperation
-            ? "Processing..."
-            : subscribed
-            ? "Unsubscribe"
-            : "Subscribe"}
-        </Button>
-      </GridItem>
-    </Grid>
-  );
+            <GridItem
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="space-between"
+                colSpan={2}
+                width="100%"
+                as="a"
+                href={channelFeed.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                title={channelFeed.name}
+            >
+                <Avatar
+                    src={props.subscription.snippet.thumbnails.default.url}
+                />
+                <Heading size="md" textAlign="center" wordBreak="break-word">
+                    {channelFeed.name}
+                </Heading>
+            </GridItem>
+            <GridItem
+                colStart={3}
+                colSpan={12}
+                display="flex"
+                flexDirection="column"
+                gap="1rem"
+                alignItems="start"
+                width="100%"
+            >
+                <Heading size="md">Most Recent Videos</Heading>
+                <Flex gap="1rem" alignItems="center" width="100%">
+                    {channelFeed.videos.slice(0, 3).map((video) => (
+                        <Video
+                            video={video}
+                            key={video.url}
+                            channelUrl={channelFeed.url}
+                        />
+                    ))}
+                </Flex>
+            </GridItem>
+            <GridItem colStart={16}>
+                <Checkbox
+                    checked={selectedSubs.includes(props.subscription.snippet.resourceId.channelId)}
+                    onChange={(ev) => {
+                        const index = selectedSubs.findIndex( (v) => v === props.subscription.snippet.resourceId.channelId);
+                        const newSubs = selectedSubs.slice();
+                        if(index !== -1) {
+                            newSubs.splice(index, 1);
+                        }
+                        else {
+                            newSubs.push(props.subscription.snippet.resourceId.channelId);
+                        }
+                        setSubs(newSubs);
+                    }}
+                >
+                    Unsubscribe
+                </Checkbox>
+            </GridItem>
+        </Grid>
+    );
 }
 
 const unsubscribeFromChannel = (subscriptionId: string) => {
-  console.log(`subscription id: ${subscriptionId}`);
-  return gapi.client.youtube.subscriptions.delete({ id: subscriptionId });
+    console.log(`subscription id: ${subscriptionId}`);
+    return gapi.client.youtube.subscriptions.delete({ id: subscriptionId });
 };
 
 const subscribeToChannel = (subscription: Subscription) => {
-  return gapi.client.youtube.subscriptions.insert({
-    part: ["id"],
-    resource: subscription,
-  });
+    return gapi.client.youtube.subscriptions.insert({
+        part: ["id"],
+        resource: subscription,
+    });
 };
 
 const useChannel = (channelId: string) => {
-  const feed = useCallback(async () => {
-    const responseText = await (
-      await fetch(`/api/channelRSS?channelId=${channelId}`)
-    ).text();
+    const feed = useCallback(async () => {
+        const responseText = await (
+            await fetch(`/api/channelRSS?channelId=${channelId}`)
+        ).text();
 
-    const data = new window.DOMParser().parseFromString(
-      responseText,
-      "text/xml"
-    );
-    const out = {
-      name: data.querySelector("feed > author > name").textContent,
-      url: data.querySelector("feed > author > uri").textContent,
-      videos: Array.from(data.querySelectorAll("entry")).map((video) => ({
-        title: video.querySelector("title").textContent,
-        url: video.querySelector("link").getAttribute("href"),
-        thumbnail: video.querySelector("thumbnail").getAttribute("url"),
-      })),
-    };
-    return out;
-  }, [channelId]);
+        const data = new window.DOMParser().parseFromString(
+            responseText,
+            "text/xml"
+        );
+        const out = {
+            name: data.querySelector("feed > author > name").textContent,
+            url: data.querySelector("feed > author > uri").textContent,
+            videos: Array.from(data.querySelectorAll("entry")).map((video) => ({
+                title: video.querySelector("title").textContent,
+                url: video.querySelector("link").getAttribute("href"),
+                thumbnail: video.querySelector("thumbnail").getAttribute("url"),
+            })),
+        };
+        return out;
+    }, [channelId]);
 
-  const { data, error } = useSWR(`/channel/${channelId}`, feed, {
-    revalidateOnFocus: false,
-    revalidateOnMount: true,
-    revalidateOnReconnect: false,
-    refreshInterval: 0,
-    refreshWhenHidden: false,
-    refreshWhenOffline: false,
-  });
+    const { data, error } = useSWR(`/channel/${channelId}`, feed, {
+        revalidateOnFocus: false,
+        revalidateOnMount: true,
+        revalidateOnReconnect: false,
+        refreshInterval: 0,
+        refreshWhenHidden: false,
+        refreshWhenOffline: false,
+    });
 
-  return { channel: data, isLoading: !data && !error, error: error };
+    return { channel: data, isLoading: !data && !error, error: error };
 };
--- public/Components/YoutubeSubscriptionManager.tsx
+++ public/Components/YoutubeSubscriptionManager.tsx
@@ -1,26 +1,77 @@
-import { VStack } from "@chakra-ui/react";
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Button, Textarea, VStack } from "@chakra-ui/react";
+import {
+    createContext,
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+} from "react";
 import { useSubscriptions } from "../Hooks/useSubscriptions";
 import YoutubeChannelListing from "./YoutubeChannelListing";
 
+export const SelectedSubscriptionContext =
+    createContext<[string[], (subs: string[]) => void]>(undefined);
+
 export default function YoutubeSubscriptionManager() {
+    if (typeof window === "undefined") return null;
 
-    if(typeof window === 'undefined')
-        return null;
+    const { subscriptions, isLoading, isError } = useSubscriptions();
 
-  const { subscriptions, isLoading, isError } = useSubscriptions();
+    const [selectedSubs, setSelectedSubs] = useState<string[]>([]);
 
-  if (isLoading) {
-    return <></>;
-  }
-  if (isError) {
-    return <>Error</>;
-  }
+    const [fetchRequest, setFetchRequest] = useState("");
 
-  return (
-    <VStack>
-      {subscriptions.map((sub) => (
-        <YoutubeChannelListing subscription={sub} key={sub.snippet.resourceId.channelId} />
-      ))}
-    </VStack>
-  );
+    const newFetchRequest = useMemo(() => {
+        const request = parseFetch(fetchRequest);
+        request.params = request.params.replace(
+            /\\?"channelIds\\?": ?\[.*\]/,
+            `\\"channelIds\\": ${JSON.stringify(selectedSubs).replaceAll(
+                '"',
+                '\\"'
+            )}`
+        );
+
+        return `fetch("${request.url}", ${request.params});`;
+    }, [fetchRequest, selectedSubs]);
+
+    if (isLoading) {
+        return <></>;
+    }
+    if (isError) {
+        return <>Error</>;
+    }
+
+    return (
+        <SelectedSubscriptionContext.Provider
+            value={[selectedSubs, setSelectedSubs]}
+        >
+            <Textarea
+                value={fetchRequest}
+                onChange={(e) => setFetchRequest(e.target.value)}
+            />
+            <Textarea value={newFetchRequest} readOnly />
+            <VStack>
+                {subscriptions.map((sub) => (
+                    <YoutubeChannelListing
+                        subscription={sub}
+                        key={sub.snippet.resourceId.channelId}
+                    />
+                ))}
+            </VStack>
+        </SelectedSubscriptionContext.Provider>
+    );
+}
+
+function parseFetch(fetchRequest: string): {
+    url: string;
+    params: string;
+} {
+    const regex = /fetch\("(.*)", ?([\s\S]*)\);/.exec(fetchRequest);
+    if (!regex) {
+        return { url: "", params: "" };
+    }
+    const url = regex[1];
+    const params = regex[2];
+    return { url, params };
 }
